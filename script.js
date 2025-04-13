34df2b29cb44cefe8d187ee64ebdd1d9092d45be
vlldnt <vieilledent.adrien@gmail.com>
2025-04-14 00:58:08 +0200

    adding scripts and css on HEADER

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll(".menu-link");
+  
+    links.forEach(link => {
+      link.addEventListener("mouseover", () => {
+        link.style.color = "#0D74CE";
+      });
+  
+      link.addEventListener("mouseout", () => {
+        link.style.color = "";
+      });
+    });
+  });
+  
