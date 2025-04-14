419a950268faf8bb281c9c9c54c399352390abe1
vlldnt <vieilledent.adrien@gmail.com>
2025-04-15 00:19:38 +0200

    update

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll(".menu-link");
-  
-    links.forEach(link => {
-      link.addEventListener("mouseover", () => {
-        link.style.color = "#0D74CE";
-      });
-  
-      link.addEventListener("mouseout", () => {
-        link.style.color = "";
-      });
+  const links = document.querySelectorAll(".menu-link");
+
+  links.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      link.style.color = "#0D74CE";
+    });
+
+    link.addEventListener("mouseout", () => {
+      link.style.color = "";
     });
   });
-  
+});
